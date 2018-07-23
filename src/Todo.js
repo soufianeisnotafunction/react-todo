057445a0675f8112154f520e7462c70189b7a50d
Soufiane Boutallaka <s.boutallaka@gmail.com>
2018-07-23 13:07:48 +0200

    updated done status style

--- src/Todo.js
+++ src/Todo.js
@@ -1,10 +1,10 @@
 import React from "react";
 
 const Todo = ({ name, click, id, done, clickDone }) => {
-  return <li className='mr-4 done-{done}'>
-      <input type="checkbox" className="mr-1" onClick={() => clickDone(id)} />
+  return <li className={`done-${done}`}>
+      <input type="checkbox" checked={done} className="mr-1" onClick={() => clickDone(id)} />
       {name}
-      <button className="btn btn-outline-danger btn-sm ml-2" onClick={() => (id)}>
+      <button className="btn btn-outline-danger btn-sm ml-2" onClick={() => click(id)}>
         X
       </button>
     </li>;
